db04e87e348fcb4a8c50508d990af5e5945e0ae3
Mathis Botteau <123393731+Froglaprod@users.noreply.github.com>
2024-10-02 11:27:15 +0200

    Exo CountFile

---
+++ Personel/Filecount/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+string pathfolder = "";
+int totalFiles = 0;
+int totalDirect = 0;
+
+Console.Write("Dossier: ");
+pathfolder = Console.ReadLine();
+countFile(pathfolder);
+
+Console.WriteLine($"{pathfolder} {totalFiles} fichiers et {totalDirect} dossiers.");
+
+void countFile(string pathfolder)
+{
+    string[] direct = Directory.GetDirectories(pathfolder);
+    int fileCount = Directory.GetFiles(pathfolder).Length;
+
+    totalFiles += fileCount;
+    totalDirect += direct.Length;
+
+    foreach (string subDir in direct)
+    {
+        countFile(subDir);
+    }
+}
